605380fe9133832e8f9ba7c61f86aed17c8acb7b
Me <davidhyppolitedh@gmail.com>
2024-02-18 20:00:44 -0500

    added Get/Post & Exceptions

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from typing import Union
+from fastapi import FastAPI, HTTPException
 
-from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -15,4 +13,16 @@ def read_root():
 @app.post('/items')
 def create_item(item: str):
     items.append(item)
-    return item
+    return items
+
+
+@app.get("/items")
+def list_items(limit: int = 10):
+    return items[0:limit]
+
+@app.get('/items/{item_id}')
+def get_item(item_id: int):
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=404, detail=(f"Item {item_id} not found"))
